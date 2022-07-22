9a59fd4fa465875aa1b3c10d459d5d5274207e95
Kin Siong Woon <108384121+KSWoon@users.noreply.github.com>
2022-07-22 14:16:06 +0800

    add login inputs
    add login inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import './App.css';
-import Header from './components/Header'
+import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 
-function App() {
+async function loginUser(credentials) {
+  console.log(credentials)
+  return fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
+
+function App({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] =  useState();
+
+  const handleSubmit = async e => {
+    console.log("hellow")
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    console.log("username")
+    setToken(token);
+  }
+
   return (
-    <div className="App">
-      <Header></Header>
+    // <Header></Header>
+    <div className="login-wrapper">
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input type="text" className="login-input" onChange={e => setUserName(e.target.value)}/>
+
+        <label>Password</label>
+        <input type="password" className="login-input" onChange={e => setPassword(e.target.value)}/>
+      </form>
+      <button type="submit" id="login-btn">Log In</button>
     </div>
   );
 }
+
+
+App.propTypes = {
+  setToken: PropTypes.func.isRequired
+};
 
 export default App;
