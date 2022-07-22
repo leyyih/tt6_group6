9159184589a0963d221c51660fd6d472839e94a2
leyyih <72239778+leyyih@users.noreply.github.com>
2022-07-22 12:09:28 +0800

    Updates frontend and Add React Bootstrap tag

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Currency App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Dashboard</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default ColorSchemesExample;
