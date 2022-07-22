c6a1569310005296dbe44f79015cec5dc624ad4c
Vejjee <Alphadam@users.noreply.github.com>
2022-07-22 12:54:39 +0800

    Display wallet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/Header'
+import Wallets from './components/Wallets'
 
 function App() {
+  
   return (
     <div className="App">
       <Header></Header>
+      <Wallets />
     </div>
   );
 }
---
+++ src/components/Wallet.js
@@ -0,0 +1,11 @@
+
+
+const Wallet = () => {
+  return (
+    <div className = 'wallet'>
+        <h3></h3>
+    </div>
+  )
+}
+
+export default Wallet
