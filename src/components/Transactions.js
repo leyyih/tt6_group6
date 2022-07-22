8dee04be5a33340077a74b14b755293c2822fa53
zyyyang <83215378+zyyyang@users.noreply.github.com>
2022-07-22 16:59:45 +0800

    adding deleted component

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -4,60 +4,6 @@ import Container from 'react-bootstrap/Container';
 import { useState } from 'react';
 
 const Transactions = ({ transactions }) => {
-    const state = {
-        originalResults: [],
-        displayResults: []
-    };
-
-    const filterResults = (query, results) => {
-        return results.filter(transaction => {
-            // const lastName = employee.lastName.toLowerCase();
-            // const firstName = employee["created_by"].toLowerCase();
-            // const fullName = firstName + " " + lastName;
-            const transactionDate = transaction["created_at"];
-
-            return transactionDate.includes(query);
-        });
-    };
-
-    const sortResults = event => {
-        this.setState(prevState => {
-            const { displayResults, sortOrder } = prevState;
-
-            if (sortOrder === "descending") {
-                displayResults.sort((a, b) => {
-                    if (a["created_by"] > b["created_by"]) {
-                        return -1;
-                    }
-                    return a["created_by"] > b["created_by"] ? 1 : 0;
-                });
-            } else {
-                displayResults.sort((a, b) => {
-                    if (a["created_by"] < b["created_by"]) {
-                        return -1;
-                    }
-                    return a["created_by"] > b["created_by"] ? 1 : 0;
-                });
-            }
-
-            return {
-                displayResults,
-                sortOrder: sortOrder === "descending" ? "ascending" : "descending"
-            };
-        });
-    };
-
-    let onChange = e => {
-        const query = e.target.value;
-
-        this.setState(prevState => ({
-            displayResults:
-                query.length > 0
-                    ? this.filterResults(query, prevState.originalResults)
-                    : prevState.originalResults
-        }));
-    };
-
     return (
         < Container className='mt-4' >
             <h3>Transactions</h3>
@@ -67,12 +13,7 @@ const Transactions = ({ transactions }) => {
                         <th>Debit</th>
                         <th>Credit</th>
                         <th>Description</th>
-                        <th
-                            style={{ cursor: "pointer" }}
-                            onClick={this.sortResults}
-                            id="date"
-                        >Date
-                        </th>
+                        <th>Date</th>
                     </tr>
                 </thead>
                 <tbody>
